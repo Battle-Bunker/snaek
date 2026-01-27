2cbb5fe730532c82a529c64ea686dd051c9e7181
Crazy_Cat911 <127078477+CrazyCat911@users.noreply.github.com>
2026-01-27 07:38:05 +0000

    remove visual testing addon from storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,13 +5,18 @@ const config: StorybookConfig = {
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|ts|svelte)"
   ],
+
   "addons": [
     "@storybook/addon-svelte-csf",
-    "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
     "@storybook/addon-docs"
   ],
-  "framework": "@storybook/sveltekit"
+
+  "framework": "@storybook/sveltekit",
+
+  core: {
+    disableWhatsNewNotifications: true
+  }
 };
 export default config;
