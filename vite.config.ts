4e45ee3cf252dc71c88cb5420a831762ad5b89f7
Crazy_Cat911 <127078477+CrazyCat911@users.noreply.github.com>
2026-01-27 08:02:00 +0000

    fix hmr on codespaces

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		hmr: {
+			clientPort: process.env.CODESPACES ? 443 : undefined
+		}
+	}
 });
