8766cb20e1382d09e7d02a1c35946b9fde018cd9
Crazy_Cat911 <127078477+CrazyCat911@users.noreply.github.com>
2026-02-19 08:21:03 +0000

    remove cursed ocdespaces fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		hmr: {
-			clientPort: process.env.CODESPACES ? 443 : undefined
-		}
-	}
+	plugins: [sveltekit()]
 });
