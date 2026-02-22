b467c3795d67a226423711ee65db1dcc2b3c3eb6
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2026-02-22 14:55:32 +1000

    add hazard and rename to cell

---
+++ src/lib/components/Cell.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	interface Props {
+		food?: boolean;
+		hazard?: boolean;
+	}
+
+	let { food = false, hazard = true }: Props = $props();
+</script>
+
+<div class={['square', hazard && 'hazard']}>
+	{#if food}
+		<div class="food"></div>
+	{/if}
+</div>
+
+<style>
+	.square {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 5em;
+		height: 5em;
+		border-radius: 1em;
+		background: #555;
+		margin: 2px;
+	}
+
+	.food {
+		width: 3em;
+		height: 3em;
+		border-radius: 100%;
+		background: #fd6b6b;
+		box-shadow: 4px 4px #444;
+	}
+
+	.hazard {
+		background: #970202;
+	}
+</style>
--- src/stories/Square.stories.ts
+++ src/stories/Square.stories.ts
@@ -1,31 +1,33 @@
 import type { Meta, StoryObj } from '@storybook/sveltekit';
 
-import Square from '$lib/components/Square.svelte';
+import Cell from '$lib/components/Cell.svelte';
 
 const meta = {
-	component: Square
-} satisfies Meta<typeof Square>;
+	component: Cell
+} satisfies Meta<typeof Cell>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Empty: Story = {};
 
-export const explicitEmpty: Story = {
-	name: 'Explicit Empty',
-	args: {
-		contents: 'empty'
-	}
-};
-
 export const Food: Story = {
 	args: {
-		contents: 'food'
+		food: true,
+		hazard: false
 	}
 };
 
 export const hazard: Story = {
 	args: {
-		contents: 'hazard'
+		food: false,
+		hazard: true
+	}
+};
+
+export const hazardFood: Story = {
+	args: {
+		food: true,
+		hazard: true
 	}
 };
